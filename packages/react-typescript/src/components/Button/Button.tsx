d0dbda7d1f9bfe5201806e7b86df86d69d7467ca
Pascal Duez <public@pascalduez.me>
2017-04-30 12:48:58 +0200

    [ts] Add TSLint

--- packages/react-typescript/src/components/Button/Button.tsx
+++ packages/react-typescript/src/components/Button/Button.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import classNames from 'classnames';
 
 interface Props {
-  label: string;
-  theme?: 'primary' | 'secondary' | 'danger';
-  handleClick?: () => void;
-};
+  label: string,
+  theme?: 'primary' | 'secondary' | 'danger',
+  handleClick?: () => void,
+}
 
 
 const Button: React.StatelessComponent<Props> = ({ label, theme, handleClick, ...extraProps }) => (
--- packages/react-typescript/src/components/Header/Header.tsx
+++ packages/react-typescript/src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 interface Props {
   title: string;
-};
+}
 
 
 const Header = ({ title, ...extraProps }: Props) => (
